0c3e639b52166e4ba75d43a5cb95a65fc4a28403
sandra8723 <sandrahiguitatoro@gmail.com>
2023-08-26 17:05:32 -0500

    26-08-2023

---
+++ App.py
@@ -0,0 +1,35 @@
+def ejercicio1():
+    print("Ejecución del ejercicio 1")
+
+def ejercicio2():
+    print("Ejecución del ejercicio 2")
+
+def ejercicio3():
+    print("Ejecución del ejercicio 3")
+
+def ejercicio4():
+    print("Ejecución del ejercicio 4")
+
+def ejercicio5():
+    print("Ejecución del ejercicio 5")
+
+print("MENU")
+print("1. Ejercicio 1")
+print("2. Ejercicio 2")
+print("3. Ejercicio 3")
+print("4. Ejercicio 4")
+print("5. Ejercicio 5")
+
+opcion = int(input("Seleccione una opción: "))
+
+
+if opcion == 1:
+    ejercicio1()
+elif opcion == 2:
+    ejercicio2()
+elif opcion == 3:
+    ejercicio3
+elif opcion == 4:
+    ejercicio4
+elif opcion == 5:
+    ejercicio5
